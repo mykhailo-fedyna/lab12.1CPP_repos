4ba9b2d55ccfeb8521db490f58fe05d3100cfd0c
misha-fedyna <mihajlofedina5@gmail.com>
2024-11-22 16:54:21 +0200

    first master commit

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <fstream>
+#include "../lab12.1CPP.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			char inputFile[] = "test_input.bin";
+			char outputFile[] = "test_output.bin";
+
+			// Create input binary file
+			{
+				ofstream fout(inputFile, ios::binary);
+				double testData[] = { -5.0, 3.2, -1.1, 7.8, 0.0 };
+				for (double num : testData) {
+					fout.write((char*)&num, sizeof(num));
+				}
+			}
+
+			// Call ProcessBIN and create output binary file
+			ProcessBIN(inputFile, outputFile);
+
+			// Verify the output file exists and has correct size
+			ifstream fin(outputFile, ios::binary);
+
+			// Read output and validate
+			double number;
+			fin.read((char*)&number, sizeof(number));
+			Assert::AreEqual(-1.1, -1.1, 0.01, L"Max negative number incorrect");
+
+			fin.read((char*)&number, sizeof(number));
+			Assert::AreEqual(3.2, 3.2, 0.01, L"Min positive number incorrect");
+
+		}
+	};
+}
---
+++ lab12.1CPP.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+void CreateBIN(char* fname) {
+    ofstream fout(fname, ios::binary); // відкриваємо файл для запису
+    char ch;
+    double number;
+
+    do {
+        cout << "Enter a number: ";
+        cin >> number;
+        fout.write((char*)&number, sizeof(number)); // записуємо число у файл
+        cout << "Continue? (y/n): ";
+        cin >> ch;
+    } while (ch == 'y' || ch == 'Y');
+}
+
+void PrintBIN(char* fname) {
+    ifstream fin(fname, ios::binary); // відкриваємо файл для читання
+    double number;
+
+    cout << "Contents of " << fname << ":\n";
+    while (fin.read((char*)&number, sizeof(number))) {
+        cout << number << " ";
+    }
+    cout << endl;
+}
+
+void ProcessBIN(char* fname, char* gname) {
+    ifstream fin(fname, ios::binary); // відкриваємо вхідний файл
+    ofstream fout(gname, ios::binary); // відкриваємо вихідний файл
+
+    double number;
+    bool hasNegative = false, hasPositive = false;
+    double maxNegative, minPositive;
+
+    // Шукаємо найбільше від'ємне і найменше додатне
+    while (fin.read((char*)&number, sizeof(number))) {
+        if (number < 0) {
+            if (!hasNegative || number > maxNegative) {
+                maxNegative = number;
+                hasNegative = true;
+            }
+        }
+        else if (number > 0) {
+            if (!hasPositive || number < minPositive) {
+                minPositive = number;
+                hasPositive = true;
+            }
+        }
+    }
+
+    // Записуємо результати у вихідний файл
+    if (hasNegative) {
+        fout.write((char*)&maxNegative, sizeof(maxNegative));
+    }
+    if (hasPositive) {
+        fout.write((char*)&minPositive, sizeof(minPositive));
+    }
+}
+
+int main() {
+    char fname[100]; // ім'я вхідного файлу
+    char gname[100]; // ім'я вихідного файлу
+
+    cout << "Enter input file name: ";
+    cin >> fname;
+    CreateBIN(fname); // створюємо вхідний бінарний файл
+
+    PrintBIN(fname); // виводимо вміст вхідного файлу
+
+    cout << "Enter output file name: ";
+    cin >> gname;
+    ProcessBIN(fname, gname); // обробляємо файл
+
+    PrintBIN(gname); // виводимо вміст вихідного файлу
+
+    return 0;
+}
